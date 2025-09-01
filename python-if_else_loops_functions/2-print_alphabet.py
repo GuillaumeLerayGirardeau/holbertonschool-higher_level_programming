9b6d8f37a75bf7ee7c34dede383481e04160876d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-01 16:05:22 +0200

    fix: wrong syntax

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
-alpha = 97
-count = 1
-
-while count < 27:
-    letter = chr(alpha)
-    print(f"{letter}", end = '')
-    count += 1
-    alpha += 1
+for i in range(97, 124):
+    print("{:c}".format(i), end = "")

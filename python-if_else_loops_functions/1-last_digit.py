6ad5a5c107b24c89e80154bac8deda8d6a89394d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-01 15:13:26 +0200

    fix: wrong python coding style

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 if number < 0:
     last_digit *= -1
-    
+
 if last_digit > 5:
     result = "is greater than 5"
 elif last_digit <= 5 and last_digit != 0:
@@ -14,4 +14,3 @@ else:
     result = "is 0"
 
 print(f"Last digit of {number} is {last_digit} and {result}")
-    

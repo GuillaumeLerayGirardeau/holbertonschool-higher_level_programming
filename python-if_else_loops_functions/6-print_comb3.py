ca67e3b7e59e2fce605fdcf3879dc93415c3df16
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-02 09:58:31 +0200

    fix: now using print in string format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@
 for i in range(9):
     for y in range(i+1, 10):
         if i != 8:
-            print(f"{i}{y}", end=", ")
+            print("{:d}".format(i) + "{:d}".format(y), end=", ")
         else:
-            print(f"{i}{y}")
+            print("{:d}".format(i) + "{:d}".format(y))

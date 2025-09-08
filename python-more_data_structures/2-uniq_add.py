c4760e2e2fb8e81ec821a336607b9263645fb0d5
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-08 15:56:09 +0200

    func: 2-uniq_add.py function added

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
 
-def check_doubles(doubles, num):
-    for i in doubles:
-        if i == num:
-            return False
-    return True
-
-
 def uniq_add(my_list=[]):
 
-    doubles = []
-    result = 0
-    count = 0
-
-    for i in my_list:
-        if count == 0:
-            result += i
-            doubles.append(i)
-            count += 1
-        else:
-            if check_doubles(doubles, i):
-                result += i
-                doubles.append(i)
-
-    return result
+    single_list = set(my_list)
+    return sum(single_list)

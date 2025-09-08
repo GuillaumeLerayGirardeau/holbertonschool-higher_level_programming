905c0e8ada14bbe18e992aed4e3465ca56e5ed6b
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-08 15:08:35 +0200

    func: 1-search_replace.py function added

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-
-    new_list = my_list.copy()
-    idx = search - 1
-
-    if idx < len(my_list):
-        new_list[idx] = replace
-    return new_list
+    return list(map(lambda x: replace if x == search else x, my_list))

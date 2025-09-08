9b5e3bfb86af887a85be62de6e3027970a40f0ee
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-08 11:22:35 +0200

    func: 8-multiple_returns.py function added

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+
+    length = len(sentence)
+    if sentence != "":
+        first_letter = sentence[0]
+    else:
+        first_letter = None
+
+    return length, first_letter

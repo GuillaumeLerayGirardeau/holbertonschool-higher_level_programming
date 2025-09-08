11ac1fb096c68833b0ff9481c2fef74a06d08ef2
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-08 15:20:48 +0200

    fix: corrected version of the exercise

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-
-    if len(tuple_a) >= 1 and len(tuple_b) >= 1:
-        value_1 = tuple_a[0] + tuple_b[0]
-    elif len(tuple_a) >= 1 and len(tuple_b) < 1:
-        value_1 = tuple_a[0] + 0
-    elif len(tuple_a) < 1 and len(tuple_b) >= 1:
-        value_1 = 0 + tuple_b[0]
-    else:
-        value_1 = 0
-
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        value_2 = tuple_a[1] + tuple_b[1]
-    elif len(tuple_a) >= 2 and len(tuple_b) < 2:
-        value_2 = tuple_a[1] + 0
-    elif len(tuple_a) < 2 and len(tuple_b) >= 2:
-        value_2 = 0 + tuple_b[1]
-    else:
-        value_2 = 0
-
-    new_tuple = (value_1, value_2)
-
-    return new_tuple
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])

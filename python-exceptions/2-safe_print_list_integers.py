c3ef58cde6b892e5c34d1c6c62f840b6257e987a
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 15:56:18 +0200

    fix: wrong syntax

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if type(my_list[count]) == int:
-                print(f"{my_list[count]}", end="")
+                print("{:d}".format(my_list[count]), end="")
                 printed += 1
             count += 1
         print("")

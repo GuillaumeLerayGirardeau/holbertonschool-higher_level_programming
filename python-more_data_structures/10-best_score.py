1ee2dbd1cca1ab01c7549cb2b54a97b2ce8d4c7c
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 10:37:05 +0200

    func: 10-best_score.py function added

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    value = 0
+    name = None
+    if a_dictionary != None:
+        for i in a_dictionary:
+            if a_dictionary[i] > value:
+                value = a_dictionary[i]
+                name = i
+    return name

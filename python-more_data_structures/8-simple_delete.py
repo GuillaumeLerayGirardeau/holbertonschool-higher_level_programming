4fde11d92a3ca2aa3460b69263d4dbe1238200f9
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 10:15:34 +0200

    fix: function now uses pop

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    for i in a_dictionary:
-        if i == key:
-            del a_dictionary[key]
-            return a_dictionary
+    a_dictionary.pop(key, None)
     return a_dictionary

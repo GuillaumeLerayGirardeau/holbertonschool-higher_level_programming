49b8db03d1ef0627fa3766f7bbf7b92f6dee75a9
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 15:52:24 +0200

    fix: new version of 12-roman_to_int, now shorter

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,24 +2,20 @@
 
 def roman_to_int(roman_string):
     value = 0
-    count = 0
-    num = ""
-    numbers = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    special = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90}
-
+    last = 0
     if type(roman_string) is str:
-        while count < len(roman_string):
-            if roman_string[count] in numbers:
-                if count + 1 < len(roman_string):
-                    num += roman_string[count] + roman_string[count + 1]
-                    if count + 1 < len(roman_string) and num in special:
-                        value += special.get(num)
-                        count += 1
-                    else:
-                        value += numbers.get(roman_string[count])
-                else:
-                    value += numbers.get(roman_string[count])
-            count += 1
-            num = ""
 
+        numbers = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+
+        for i in reversed(roman_string):
+            if i in numbers:
+                if numbers.get(i) < last:
+                    value -= numbers.get(i)
+                else:
+                    value += numbers.get(i)
+                last = numbers.get(i)
+    
     return value

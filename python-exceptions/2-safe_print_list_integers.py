f83d684ba62ba8326355bc1d2eb85ec9379769c3
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 15:02:23 +0200

    func: 2-safe_print_list_integers.py function added

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    printed = 0
+    try:
+        for i in range(x):
+            if type(my_list[count]) == int:
+                print(f"{my_list[count]}", end="")
+                printed += 1
+            count += 1
+        print("")
+        return printed
+    except:
+        return

818d963d569fec77438d54e44b57116ad7cb139f
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 10:49:43 +0200

    fix: corrected version of the function

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    value = 0
-    name = None
-    if a_dictionary is not None:
-        for i in a_dictionary:
-            if a_dictionary[i] > value:
-                value = a_dictionary[i]
-                name = i
-    return name
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
+    return None

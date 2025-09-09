2afc04f2cf9fdd71a2563ccd0327c40eba82abf5
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 10:38:09 +0200

    fix: better pycode style

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     value = 0
     name = None
-    if a_dictionary != None:
+    if a_dictionary is not None:
         for i in a_dictionary:
             if a_dictionary[i] > value:
                 value = a_dictionary[i]

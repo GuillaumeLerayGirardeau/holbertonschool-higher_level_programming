1b58ace87e2bb2254bba52203337d1dfb8178839
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 09:49:00 +0200

    func: 6-print_sorted_dictionary.py function added

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    new_dic = dict(sorted(a_dictionary.items()))
+    for key, value in new_dic.items():
+        print(f"{key}: {value}")

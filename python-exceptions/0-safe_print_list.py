fbfb344f098dc45c13c9202ca928204828dc3338
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-09 14:38:10 +0200

    0-safe_print_list.py function added, ReadMe added

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        while count < x:
+            print(f"{my_list[count]}", end="")
+            count += 1
+        print("")
+        return count
+    except:
+        print("")
+        return count

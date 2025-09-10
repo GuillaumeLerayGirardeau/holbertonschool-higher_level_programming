7ab8f8c2bf0be0329af528854d8c8a9e16eeaaf9
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-10 12:01:25 +0200

    fix: wrong syntax

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,6 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: " + "{num}".format(num = result))
-
+        print("Inside result: " + "{}".format(result))
     return result

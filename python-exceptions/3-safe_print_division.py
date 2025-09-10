ff551edd329446118f912d21c022dff6045e58c6
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-10 10:36:53 +0200

    fix: updated except error triggers

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: " + "{num}".format(num = result))

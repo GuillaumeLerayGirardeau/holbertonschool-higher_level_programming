ae5c396d8dc3e3c85b494257deb6339dc5931bbd
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-10 11:27:10 +0200

    fix: wrong error

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    count = 0
     printed = 0
-    try:
-        for i in range(x):
-            if type(my_list[count]) == int:
-                print("{:d}".format(my_list[count]), end="")
-                printed += 1
-            count += 1
-        print("")
-        return printed
-    except IndexError:
-        return
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            printed += 1
+        except (TypeError, IndexError, ValueError) as e:
+            if isinstance(e, IndexError):
+                return
+            else:
+                continue
+    print("")
+    return printed

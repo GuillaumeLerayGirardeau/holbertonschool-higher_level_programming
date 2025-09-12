7099004646a7db36f5aaf518a3c18b89fab2647b
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-12 11:10:12 +0200

    fix: updated doc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,15 +1,16 @@
+"""
+Adds two integers
+
+Args:
+    a (int): first number
+    b (int): second number
+
+Returns: sum of the two integer
+"""
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
-    """
-    Adds two integers
-
-    Args:
-        a (int): first number
-        b (int): second number
-
-    Returns: sum of the two integer
-    """
+   
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):

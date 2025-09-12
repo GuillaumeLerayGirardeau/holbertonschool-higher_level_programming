4fb293365036d2c6b87c3f093ae6c7f7e67abb64
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-12 11:12:16 +0200

    fix: updated doc

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,6 +10,12 @@ Returns: sum of the two integer
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+
+    """
+    Adds two integers
+    Take two integer for arguments
+    Return the result, or TypeError
+    """
    
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")

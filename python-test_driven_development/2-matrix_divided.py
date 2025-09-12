a5ad69e11efb0858774fdb434032fa6466b36a3b
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-12 15:04:36 +0200

    func: matrix_divided function added

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,33 @@
+"""
+Module Matrix divided
+
+Functions:
+    matrix_divided(matrix, div)
+"""
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    """
+    divide all elements of a matrix
+    matrix must be a list of lists of integers or floats
+    each rows of the matrix must have the same size
+    div must be a number (integer or float) and can't be equal to 0
+    return a new matrix, else TypeError or ZeroDivisionError
+    """
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    elif not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    try:
+        new_mat = []
+        for i in matrix:
+            new_mat.append(list(map(lambda x: (x / div).__round__(2), i)))
+        return new_mat
+    except (TypeError, ValueError) as e:
+        if e == TypeError:
+            raise TypeError("matrix must be a matrix (list of lists) of " \
+            "integers/floats")
+        else:
+            raise TypeError("matrix must be a matrix (list of lists) of " \
+            "integers/floats")

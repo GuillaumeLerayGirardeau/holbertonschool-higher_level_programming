35f30499b9ed69d9327893c37713e6bd83837c7f
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-12 11:28:24 +0200

    fix: now handle single names

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -15,9 +15,9 @@ def say_my_name(first_name, last_name=""):
     Return the ful string, or TypeError
     """
 
-    if not isinstance(first_name, str) or first_name == "":
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    elif not isinstance(last_name, str) or last_name == "":
+    elif not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     
     print(f"My name is {first_name} {last_name}")

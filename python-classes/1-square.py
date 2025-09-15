cd855d9d818357f4bfee4dc93e7ae85e9545448d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-15 14:35:08 +0200

    func: added the module 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This module define a Square class
+"""
+
+
+class Square:
+    """
+    Define a Square class
+    Square class having a private size attribute
+    Don't forget the uppercase character
+    """
+    def __init__(self, size):
+        self.__size = size

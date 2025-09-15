5532d2eb23208e6316fbbc803d4d7219bb0eddf1
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-15 14:52:48 +0200

    func: 2-square module added

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module define a Square class
+"""
+
+
+class Square:
+    """
+    Define a Square class
+    Square class having a private size attribute
+    Don't forget the uppercase character
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif not self.__size >= 0:
+            raise ValueError("size must be >= 0")

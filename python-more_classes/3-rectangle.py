d06567a1330ce696314dc7d1a0bd610094dc6446
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-16 10:37:24 +0200

    func: 3-rectangle module added, contains new methods to print rectangle

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+"""
+This module contains the class Rectangle
+"""
+
+
+class Rectangle:
+
+    """
+    Create the Rectangle class
+    contains several methods about rectangle
+    don't forget the uppercase character
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return self.__height * 2 + self.__width * 2
+
+    def __str__(self):
+        string = ""
+        if self.perimeter == 0:
+            return (string)
+        else:
+            for i in range(self.__height):
+                for y in range(self.__width):
+                    string = string + "#"
+                if i != self.__height - 1:
+                    string = string + "\n"
+            return (string)

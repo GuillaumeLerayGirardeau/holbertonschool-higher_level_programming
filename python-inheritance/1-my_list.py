32f1bfe53cb3d19d8c93e99c10d45f424037e51f
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-22 11:59:05 +0200

    func: class MyList added with tes file

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+Class MyList
+"""
+
+
+class MyList(list):
+
+    """
+    class MyList, inherits from list
+    """
+
+    def print_sorted(self):
+        print(sorted(self))

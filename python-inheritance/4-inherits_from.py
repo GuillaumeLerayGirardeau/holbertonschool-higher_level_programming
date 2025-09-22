c392801fff5b795bac402321d133339c077eb5ac
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-22 15:22:00 +0200

    func: inherits_from documentation added

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+function inherits_from
+"""
+
 
 def inherits_from(obj, a_class):
+    """
+    checks if obj is a subclass of a_class
+    return True if it is, else false
+    """
     return issubclass(type(obj), a_class) and type(obj) is not a_class

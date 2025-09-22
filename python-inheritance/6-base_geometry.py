a4b541d9f330f4a3789ecc82545b78f3e1f28cde
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-22 13:56:49 +0200

    func: BaseGeometry updated

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    don't forget the double uppercase characters
+    """
+    def area(self):
+        raise Exception("area() is not implemented")

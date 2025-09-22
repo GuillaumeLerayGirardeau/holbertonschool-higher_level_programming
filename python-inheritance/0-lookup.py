a9c0f47a03c8b369788a8f1e7b067503dac68534
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-22 10:43:49 +0200

    fix: corrected pycode style

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -8,6 +8,6 @@ function lookup
 def lookup(obj):
 
     """
-    return a list of all the methods and attributes available 
+    return a list of all the methods and attributes available
     """
     return (list(dir(obj)))

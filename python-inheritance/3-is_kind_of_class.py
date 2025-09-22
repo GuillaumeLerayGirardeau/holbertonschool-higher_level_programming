5072747aced54e80c52956e40b4c24fbae6b3c97
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-22 13:30:59 +0200

    func: is_kind_of_class function added

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+function is_kind_of_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    checks if the object is an instance of a class
+    return true if it is, else false
+    """
+    return (isinstance(obj, a_class))

b95307385ee454a33bc1a55048e4feee587975a9
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-22 13:17:12 +0200

    func: is_same_class function added

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+function is_same_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checks if obj is an instance of the class a_class
+    return true if it is, else false
+    """
+    return (isinstance(obj, a_class))

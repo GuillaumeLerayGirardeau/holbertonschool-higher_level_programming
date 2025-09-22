869e4cc40d6761183ed823339576cbc9d2e0fccc
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-22 14:59:03 +0200

    func: rectangle class added, inherits from BaseGeometry class

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    don't forget the double uppercase characters
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int) or isinstance(value, bool):
+            raise TypeError(f"{name} must be an integer")
+        elif not value > 0:
+            raise ValueError(f"{name} must be greater than 0")
+        else:
+            return value
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class, inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        __width = super().integer_validator("width", width)
+        __height = super().integer_validator("height", height)

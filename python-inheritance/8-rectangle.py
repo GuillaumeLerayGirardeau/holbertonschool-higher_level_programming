d422ff2d8bfc9212cb7e8d7bb95e2f44af9224b2
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-22 15:24:11 +0200

    fix: better documentation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 
 """
 BaseGeometry class
+Rectangle class
 """
 
 
@@ -27,6 +28,7 @@ class Rectangle(BaseGeometry):
     Rectangle class
     don't forget the uppercase character
     """
+
     def __init__(self, width, height):
         __width = super().integer_validator("width", width)
         __height = super().integer_validator("height", height)

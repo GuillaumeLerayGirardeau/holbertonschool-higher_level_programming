6b3af72e2c98992022bc5daed473e0676393438b
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-23 10:32:06 +0200

    fix: now import rectangle class

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,6 +6,7 @@ Square class
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 Rectangle = __import__('9-rectangle').Rectangle
 
 
@@ -16,7 +17,12 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        self.__size = self.integer_validator("size", size)
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         return self.__size * self.__size
+
+    def __str__(self):
+        return (f"[Rectangle] {self.__size}/{self.__size}")

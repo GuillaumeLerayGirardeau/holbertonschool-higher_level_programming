bad3881d6305c3182096d9d44a10e64899e3260a
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-23 10:06:06 +0200

    fix: now import class BaseGeometry

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python3
-
 """
 BaseGeometry class
 Rectangle class
 """
 
 
-class BaseGeometry:
-    """
-    BaseGeometry class
-    don't forget the double uppercase characters
-    """
-    def area(self, __width, __height):
-        return __width * __height
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or isinstance(value, bool):
-            raise TypeError(f"{name} must be an integer")
-        elif not value > 0:
-            raise ValueError(f"{name} must be greater than 0")
-        else:
-            return value
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):

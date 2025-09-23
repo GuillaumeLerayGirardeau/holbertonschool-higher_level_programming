ae8f8e822c0b31d0a35b0bdb16e0f71580d4f064
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-23 11:20:57 +0200

    func: task 01 added, can return area and perimeter of multiple shapes

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+Class Shape and subclasses that calculate perimeter and area of shapes
+"""
+
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    """
+    Abstract class that define operation to implement
+    """
+
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    """
+    Can set and return area and perimeter of a circle
+    """
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return self.radius * self.radius * pi
+
+    def perimeter(self):
+        return 2 * pi * self.radius
+
+
+class Rectangle(Shape):
+    """
+    Can set and return area and perimeter of a rectangle
+    """
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return self.width * 2 + self.height * 2
+
+
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")

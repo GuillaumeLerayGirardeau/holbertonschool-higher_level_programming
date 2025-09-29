e6cb0c47d43292314ae073c7ee190da304f252e1
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-29 14:06:24 +0200

    test: test export

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,7 +2,6 @@
 """
 return the dictionnary descritpion with simple data structure
 """
-import json
 
 
 def class_to_json(obj):
@@ -10,4 +9,4 @@ def class_to_json(obj):
     returns the dictionary description with simple data structure
     for JSON serialization of an object
     """
-    return json.dump(obj)
+    pass

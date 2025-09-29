f2bad11c15f364b709ef34c5976a38a76b38b8a5
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-29 11:41:10 +0200

    fix: now read a string instead of a file

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -10,4 +10,4 @@ def load_from_json_file(filename):
     creates an Object from a JSON file
     """
     with open(filename) as file:
-        return json.load(file.read())
+        return json.loads(file.read())

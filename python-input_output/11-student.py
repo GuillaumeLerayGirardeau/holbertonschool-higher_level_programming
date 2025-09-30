c7ea5c146242bb5e010d087f7ede1971af88f24a
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-30 10:09:34 +0200

    fix: wrong output

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -26,5 +26,4 @@ class Student:
 
     def reload_from_json(self, json):
         for i in self.__dict__:
-            if i in json.__dict__:
-                self.__dict__[i] = json[i]
+            self.__dict__[i] = json[i]

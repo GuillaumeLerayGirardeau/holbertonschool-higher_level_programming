0b0f66a7fc1a0d2970020aa61e8220633e93e659
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-30 10:01:27 +0200

    func: student.py class updated with reload_from_json method

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student:
+    """
+    Student Class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            dict_attribs = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    dict_attribs.update({i: getattr(self, i)})
+            return dict_attribs
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]

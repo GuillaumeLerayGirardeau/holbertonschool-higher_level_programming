87d12fe258fc140054da4c11597c04673a1e3d3e
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-09-30 10:07:01 +0200

    fix: reload_from_json now handle all attributes

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -25,6 +25,5 @@ class Student:
             return dict_attribs
 
     def reload_from_json(self, json):
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        for i in self.__dict__:
+            self.__dict__[i] = json[i]

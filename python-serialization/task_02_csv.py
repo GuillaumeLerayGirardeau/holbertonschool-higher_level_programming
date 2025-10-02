ec184f978ed96773a5566cb048a11761522737fd
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-02 11:19:15 +0200

    fix: corrected pycode style

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""
+Function convert_csv_to_json
+"""
 
-import csv 
+import csv
 import json
 
+
 def convert_csv_to_json(csv_file):
+    """
+    This function convert csv file to json file
+    """
     with open("data.json", "w") as file:
         try:
             json.dump(csv.DictReader(csv_file), file)
             return True
-        except:
+        except FileNotFoundError:
             return False

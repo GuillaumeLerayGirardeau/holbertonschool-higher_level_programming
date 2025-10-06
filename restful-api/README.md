36d2c63dc46b165cf4d8e484542b26822308f59c
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-06 14:58:50 +0200

    task_02 function added, ReadMe added to the new folder

---
+++ restful-api/README.md
@@ -0,0 +1,3 @@
+# High Level Programming
+
+This repository contains exercises about RESTful API with Python programming language.
---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    post = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code : {post.status_code}")
+    if post.status_code == 200:
+        data = post.json()
+        for i in data:
+            print(i["title"])
+
+
+def fetch_and_save_posts():
+    post = requests.get("https://jsonplaceholder.typicode.com/posts")
+    data = post.json()
+    post_dict = []
+    new_post = {}
+
+    for i in data:
+        new_post["id"] = i["id"]
+        new_post["title"] = i["title"]
+        new_post["body"] = i["body"]
+        post_dict.append(new_post.copy())
+
+    with open("posts.csv", "w") as file:
+        fields = ["id", "title", "body"]
+        writer = csv.DictWriter(file, fieldnames=fields)
+        writer.writeheader()
+        writer.writerows(post_dict)

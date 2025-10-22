70f27693a5785479659b5b3397ffa58f0bf6673d
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-22 10:27:01 +0200

    fix: changed column name

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,6 +1,6 @@
 -- Lists all genres linked to the show 'Dexter'
 
-SELECT tv_genres.name AS genre
+SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id

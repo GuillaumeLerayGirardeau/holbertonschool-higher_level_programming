58a81bcee2563f0e9880b51ea97ffb0527d8fd7b
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-23 11:28:39 +0200

    add: script added that lists all cities with a name started by 'N'

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+"""
+Lists all states with a name starting with N from the database hbtn_0e_0_usa
+"""
+
+if __name__ == "__main__":
+
+    i = 0
+    arguments = sys.argv[1:]
+
+    if len(arguments) != 3:
+        print("Invalid number of arguments !")
+        exit()
+
+    try:
+        db_connection = MySQLdb.connect(
+            host="localhost",
+            user=arguments[0],
+            password=arguments[1],
+            db=arguments[2],
+            port=3306
+        )
+    except Exception as e:
+        print("Can't connect to the database :", e)
+        exit()
+
+    cursor = db_connection.cursor()
+    states_num = cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
+        )
+    while i < states_num:
+        m = cursor.fetchone()
+        print(m)
+        i += 1
+
+    cursor.close()
+    db_connection.close()

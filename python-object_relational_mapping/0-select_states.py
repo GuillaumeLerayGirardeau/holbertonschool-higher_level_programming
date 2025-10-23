1552f4554fbb5bd0337a3c8c04536aac91cc35af
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-23 11:17:16 +0200

    fix: corrected pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-        
+
     i = 0
     arguments = sys.argv[1:]
 
@@ -18,23 +18,22 @@ if __name__ == "__main__":
 
     try:
         db_connection = MySQLdb.connect(
-            host = "localhost",
-            user = arguments[0], 
-            password = arguments[1],
-            db = arguments[2],
-            port = 3306
-            )
+            host="localhost",
+            user=arguments[0],
+            password=arguments[1],
+            db=arguments[2],
+            port=3306
+        )
     except Exception as e:
         print("Can't connect to the database :", e)
         exit()
-    
 
     cursor = db_connection.cursor()
     states_num = cursor.execute("SELECT * FROM states")
-    while i < states_num :
+    while i < states_num:
         m = cursor.fetchone()
         print(m)
-        i+=1
+        i += 1
 
     cursor.close()
     db_connection.close()

e69bac335d651ba0311b094958083ab540928935
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-23 11:14:16 +0200

    add: ReadMe file added, script added that lists all states from the database

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+        
+    i = 0
+    arguments = sys.argv[1:]
+
+    if len(arguments) != 3:
+        print("Invalid number of arguments !")
+        exit()
+
+    try:
+        db_connection = MySQLdb.connect(
+            host = "localhost",
+            user = arguments[0], 
+            password = arguments[1],
+            db = arguments[2],
+            port = 3306
+            )
+    except Exception as e:
+        print("Can't connect to the database :", e)
+        exit()
+    
+
+    cursor = db_connection.cursor()
+    states_num = cursor.execute("SELECT * FROM states")
+    while i < states_num :
+        m = cursor.fetchone()
+        print(m)
+        i+=1
+
+    cursor.close()
+    db_connection.close()
---
+++ python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+# Object Relational Mapping
+
+This repository contains exercises about object relational mapping with Python.

fb96b1e2209e9ef1f945df7f64071fe01a49bfc9
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-24 10:58:01 +0200

    fix: added Integer

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -4,7 +4,7 @@
 python file that contains the class definition of a State
 """
 
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -15,5 +15,5 @@ class State(Base):
     Class that represents the table states
     """
     __tablename__ = "states"
-    id = Column(autoincrement=True, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)

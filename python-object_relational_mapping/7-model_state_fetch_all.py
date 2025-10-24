7006395b36ae3792a587b948c729a147799bf156
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-24 11:09:33 +0200

    add: script added that lists all states objects

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+Lists all states objects
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+import sys
+
+arg = sys.argv[1:]
+
+engine = create_engine(
+    f"mysql+mysqldb://{arg[0]}:{arg[1]}@localhost:3306/{arg[2]}", 
+    pool_pre_ping=True)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+states = session.query(State).all()
+
+for state in states:
+    print(f"{state.id}: {state.name}")

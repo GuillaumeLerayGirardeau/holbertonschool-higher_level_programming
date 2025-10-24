a407615effc5e29ee317278f68d31b01c3c704c4
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-10-24 10:52:46 +0200

    add: script added that creates a python file that contains the class definition of a State

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+python file that contains the class definition of a State
+"""
+
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)

13f33fe44b7beca5c6f9ab0283ef977660704d99
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-07 12:23:37 +0100

    fix: can now handle negative numbers

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 /* Prints x times "C is fun" */
 
 let printNumber = parseInt(process.argv[2]);
-if (isNaN(printNumber)) {
+if (isNaN(printNumber) || printNumber < 0) {
   console.log('Missing number of occurrences');
 } else {
   while (printNumber !== 0) {

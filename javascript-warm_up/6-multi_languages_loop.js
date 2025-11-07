9cffdc238102a502b68c3055da8d3659aaea5c3b
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-07 12:11:01 +0100

    add: file 6 added

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+/* Prints 3 lines but by using an array of string and a loop */
+
+const stringArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let counter = 0; counter < stringArray.length; counter++) {
+  console.log(stringArray[counter]);
+}

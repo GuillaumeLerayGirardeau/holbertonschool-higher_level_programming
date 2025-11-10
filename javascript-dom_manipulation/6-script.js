2aea8d23c4152bed0ded280e10a1f2c5cc2ce912
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-10 14:59:12 +0100

    add: file 6 added

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+/* Fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json */
+
+fetchCharacter();
+
+async function fetchCharacter () {
+  const response = await fetch('https://swapi-api.hbtn.io/api/people/5/?format=json');
+  const charaName = await response.json();
+  const starWarsName = charaName.name;
+  const character = document.getElementById('character');
+  character.innerHTML = starWarsName;
+}

f0ad1780db74f007a29328b82b4be8b87c1e8cb0
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-13 13:41:21 +0100

    fix: syntax error

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,50 +1,46 @@
 #!/usr/bin/python3
 
-
 import os
-
-"""
-Template function that generates invitations
-"""
 
 
 def generate_invitations(template, attendees):
-    """
-    Python function that generate a invitation
-    """
+    ''' a Python function that generates personalized invitation files from a
+    template with placeholders and a list of objects.
+    Arguments:
+    template: text with placeholders
+    attendees: a list of dictionaries
+    return: Error on failure, nothing on success.'''
 
-    if template is None:
-        print("Template is empty, no output files generated.")
-        return
-    elif not isinstance(template, str) or not isinstance(attendees, list):
-        print("Template must be a string and attendees a list of dictionaries")
-        return
-    elif attendees is None:
-        print("No data provided, no output files generated.")
+    if not isinstance(template, str) or not isinstance(attendees, list):
+        print('template must be a string and attendees a list of'
+              'dictionaries.')
         return
 
-    needed_keys = ["name", "event_title", "event_date", "event_location"]
+    elif len(template) <= 0:
+        print('Template is empty, no output files generated.')
+        return
 
-    for dictionary in attendees:
-        if not isinstance(dictionary, dict):
-            print("Attendees must be a list of dictionaries")
+    elif len(attendees) <= 0:
+        print('No data provided, no output files generated.')
+        return
+
+    dict_model = ['name', 'event_title', 'event_date', 'event_location']
+
+    for invite in attendees:
+        if not isinstance(invite, dict):
+            print('template must be a string and attendees a list'
+                  ' of dictionaries.')
             return
-        for key in dictionary:
-            if key not in needed_keys or dictionary[key] is None:
-                dictionary[key] = "N/A"
-
-    for dictionary in attendees:
-        for key in dictionary:
-            if dictionary[key] is None:
-                dictionary[key] = "N/A"
-
+        for key in dict_model:
+            if key not in invite or invite[key] is None:
+                invite[key] = 'N/A'
     try:
-        invit_number = 1
+        i = 0
         for attendee in attendees:
-            with open(f"output_{invit_number}.txt", "w") as f:
-                f.write(template.format(**attendee))
+            i += 1
+            with open(
+                    f'output_{i}.txt', 'w', encoding='utf-8') as invitation:
+                invitation.write(template.format(**attendee))
 
-            invit_number += 1
-
-    except Exception as e:
-        print(e)
+    except Exception:
+        raise

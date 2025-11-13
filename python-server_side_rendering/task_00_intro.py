3d6cd47c481870eada93e3b4f44fd328ab253cbe
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-11-13 13:27:06 +0100

    fix: corrected pycodestyle

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
+
 import os
 
 """
 Template function that generates invitations
 """
+
 
 def generate_invitations(template, attendees):
 
@@ -22,36 +25,36 @@ def generate_invitations(template, attendees):
     else:
         for element in attendees:
             if type(element) is not dict:
-                print(f"Error: attendees is a list with {type(element)} and not only dictionaries")
+                print(f"Error: attendees is a list with \
+                      {type(element)} and not only dictionaries")
                 return
-            
+
     needed_keys = ["name", "event_title", "event_date", "event_location"]
 
     for dictionary in attendees:
         for keys in needed_keys:
-            if not keys in dictionary:
+            if keys not in dictionary:
                 dictionary[keys] = "N/A"
         for key in dictionary:
-            if dictionary[key] == None:
+            if dictionary[key] is None:
                 dictionary[key] = "N/A"
-    
+
     try:
         invit_number = 1
         for i in attendees:
             new_template = template
             new_template = new_template.replace("{name}", i["name"])
-            new_template = new_template.replace("{event_title}", i["event_title"])
-            new_template = new_template.replace("{event_date}", i["event_date"])
-            new_template = new_template.replace("{event_location}", i["event_location"])
+            new_template = new_template.replace(
+                "{event_title}", i["event_title"])
+            new_template = new_template.replace(
+                "{event_date}", i["event_date"])
+            new_template = new_template.replace(
+                "{event_location}", i["event_location"])
 
-            if os.path.exists(f'output_{invit_number}.txt'):
-                with open(f"output_{invit_number}.txt", "w") as f:
-                    f.write(new_template)
-            else:
-                with open(f"output_{invit_number}.txt", "x") as f:
-                    f.write(new_template)
+            with open(f"output_{invit_number}.txt", "w") as f:
+                f.write(new_template)
 
             invit_number += 1
-        
-    except Exception as e :
+
+    except Exception as e:
         print(e)
